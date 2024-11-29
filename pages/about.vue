0f389a284f1a228295228f2869ead46b947979b9
Yetta <yetta@supergeo.com.tw>
2024-11-29 18:02:32 +0800

    建立元件、建立函式庫

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts">
+<script setup>
 definePageMeta({
   layout: "admin-layout",
 });
+const title = ref("計數器");
+const { count, increment, decrement } = useCounter();
 </script>
 
 <template>
-  <div>Page: about</div>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>現在的計數器值：{{ count }}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
 </template>
 
 <style scoped></style>

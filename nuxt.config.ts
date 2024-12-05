9a4bb7ae01d570679ba11ee80b1cb28eafb7a919
Yetta <yetta@supergeo.com.tw>
2024-12-05 15:11:28 +0800

    設定head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,38 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app:{
+    head: {
+      title: 'Nuxt project',
+      
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'My Nuxt web' },
+        /*分享設定*/
+        { property: "og:title", content: "Nuxt3 Day11  Global head 練習" },
+        { property: "og:url", content: "http://localhost:3000/" },
+        {
+          property: "og:description", content: "透過今天的學習，將會學習到 Nuxt3  Global head  的設定方法",
+        },
+        {property:"og:image",content:"https://images.unsplash.com/photo-1498050108023-c5249f4df085"}
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+         /*文字*/
+         { rel: "preconnect", href: "https://fonts.googleapis.com" },
+         {
+           rel: "preconnect",
+           href: "https://fonts.gstatic.com",
+           crossorigin: "",
+         },
+         {
+           rel: "stylesheet",
+           href: "https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap",
+         },
+      ],
+    },
+  },
   
   css:[
     '@/assets/stylesheets/all.scss',

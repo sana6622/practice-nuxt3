868e29afbe6bee736c4270d2ac7dc7653d70f51e
Yetta <yetta@supergeo.com.tw>
2025-01-13 17:49:41 +0800

    install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+};
+export default config;

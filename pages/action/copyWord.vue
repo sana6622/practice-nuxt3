bdf4ae5db226fbef9110234c6567c14e2f8b1856
Yetta <yetta@supergeo.com.tw>
2025-01-24 15:42:53 +0800

    feat: 複製文字

--- pages/action/copyWord.vue
+++ pages/action/copyWord.vue
@@ -1,12 +1,11 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <div class="copy-word">
     <h1>點擊後可以複製文字</h1>
-    <p v-copy class="copy">點擊這段文字後可以複製</p>  
-     <div class="hit">
+    <p v-copy class="copy">點擊這段文字後可以複製</p>
+    <div class="hit">
+      <p>安裝 : npm i clipboard</p>
       <p>使用檔案: plugin/clipboard.js</p>
       <p>使用技巧: plugin -> directive (自定義) , binding</p>
     </div>
@@ -15,19 +14,18 @@
 
 <style lang="scss" scoped>
 .copy-word {
-   .copy{
+  .copy {
     padding: 10px;
     background-color: rgb(255, 244, 145);
     display: inline-block;
     cursor: pointer;
   }
-.hit{ 
-  background-color: pink;
-  padding: 15px;
-  p{
-    font-size: 18px;
+  .hit {
+    background-color: pink;
+    padding: 15px;
+    p {
+      font-size: 18px;
+    }
   }
 }
-}
-
 </style>
--- plugins/clipboard.js
+++ plugins/clipboard.js
@@ -1,20 +1,39 @@
+import ClipboardJS from "clipboard";
 export default defineNuxtPlugin((nuxtApp) => {
   //使用directive 建立點擊後複製的功能
-  nuxtApp.vueApp.directive('copy',{
+  nuxtApp.vueApp.directive("copy", {
     /*只在客戶端執行*/
-    mounted(el) {
-      el.addEventListener('click', (e) => {
-        navigator.clipboard.writeText(e.target.innerText);
-        alert('成功複製內容')
-      })
+    mounted(el, binding) {
+      // 使用 clipboard.js 將點擊元素與複製功能綁定
+      const clipboard = new ClipboardJS(el, {
+        text: () => binding.value || el.innerText, // 若有傳遞值則使用，否則默認使用內部文字
+      });
+
+      clipboard.on("success", () => {
+        alert("複製成功：" + (binding.value || el.innerText));
+      });
+
+      clipboard.on("error", () => {
+        alert("複製失敗，請檢查瀏覽器支援");
+      });
+
+      // 將 clipboard 實例存儲於元素，方便在 unmounted 時銷毀
+      el._clipboardInstance = clipboard;
+    },
+    unmounted(el) {
+      // 清除綁定事件，防止內存洩漏
+      if (el._clipboardInstance) {
+        el._clipboardInstance.destroy();
+        delete el._clipboardInstance;
+      }
     },
     /*只在伺服器端執行*/
     // getSSRProps(binding){ /*複製的文字會放在binding.value*/
     //   console.log('getSSRPro binding',binding)
-      
+
     //   return {
     //     title: binding.value
     //   }
     // }
-  })
-})
+  });
+});

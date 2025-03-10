91c86a77e5a53e8f037323d3a4993447f1691a42
Yetta <yetta@supergeo.com.tw>
2025-03-10 18:06:18 +0800

    刪除無法使用的登入套件

--- plugins/googleAuth.js
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from "#app";
-import GoogleLogin from "vue3-google-login";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) return; // 確保只在瀏覽器端執行
-  nuxtApp.vueApp.use(GoogleLogin, {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-  });
-});
